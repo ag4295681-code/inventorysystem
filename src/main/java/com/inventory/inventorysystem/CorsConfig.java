93447904762eea066823e26dbcfcb592b9bc88a3
ag4295681-code <ag4295681@gmail.com>
2026-06-25 20:50:29 +0530

    update

--- src/main/java/com/inventory/inventorysystem/CorsConfig.java
+++ src/main/java/com/inventory/inventorysystem/CorsConfig.java
@@ -9,7 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins(
+                    "http://localhost:3000",
+                    "http://127.0.0.1:5500",
+                    "http://localhost:5500"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
